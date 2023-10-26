deb939b8fe4695bd7f43cea256a475df90d5d413
duncanlaidlaw <slamdunc123@gmail.com>
2023-10-26 14:21:13 +0100

    feature - added component state to change burger menu icon

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,14 +1,26 @@
+'use client';
+
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { MdOutlineMenu } from 'react-icons/md';
+import { MdOutlineClose } from 'react-icons/md';
 
 const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleMenuOnClick = () => {
+		setIsOpen((prevIsOpen) => !prevIsOpen);
+	};
 	return (
 		<nav className='bg-gray-200 flex w-full h-12 justify-between items-center'>
 			<div className='w-32 p-2'>360dronepro</div>
-			<div className='p-2 sm:hidden'>Burger</div>
+			<button className='p-2 sm:hidden' onClick={handleMenuOnClick}>
+				{!isOpen ? <MdOutlineMenu /> : <MdOutlineClose />}
+			</button>
 			<div className='w-96 p-2 hidden sm:block'>
 				<ul className='flex justify-between'>
 					<Link href='/'>Home</Link>
-					<Link href='/holidaylets'>Holiday Lets</Link>
+					<Link href='/holiday-lets'>Holiday Lets</Link>
 					<Link href='/rentals'>Rentals</Link>
 					<Link href='/sales'>Sales</Link>
 				</ul>
