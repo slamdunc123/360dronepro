2da44131486b531837cea7841778051f568e4109
duncanlaidlaw <slamdunc123@gmail.com>
2023-10-26 11:01:09 +0100

    fix - height of iframe

--- src/app/tour/[id]/page.tsx
+++ src/app/tour/[id]/page.tsx
@@ -7,7 +7,7 @@ const Page = ({ params }: { params: { id: string } }) => {
 	const tour = tours.find((item) => item.id === params.id);
 
 	return (
-		<div className='h-5/6 w-full xl:w-10/12 overflow-auto'>
+		<div className='h-[90%] w-full xl:w-10/12'>
 			<iframe
 				width='100%'
 				height='100%'
