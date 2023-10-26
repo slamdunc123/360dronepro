e8d46087ef8527688911fde1e690ed6fa0e4a899
duncanlaidlaw <slamdunc123@gmail.com>
2023-10-26 17:48:51 +0100

    fix - added correct query params to enable zoom

--- src/app/config.js
+++ src/app/config.js
@@ -2,21 +2,21 @@ export const tours = [
 	{
 		id: '1',
 		name: 'Rock Cottage',
-		url: 'https://kuula.co/share/collection/7JB7p?logo=-1&info=0&fs=0&vr=0&gyro=0&initload=0&thumbs=1&keys=0',
+		url: 'https://kuula.co/share/collection/7JB7p?logo=-1&info=0&fs=0&vr=0&zoom=1&gyro=0&initload=0&thumbs=1&keys=0',
 	},
 	{
 		id: '2',
 		name: 'Dale View',
-		url: 'https://kuula.co/share/collection/7J2HW?logo=-1&info=0&fs=0&vr=0&gyro=0&initload=0&thumbs=1&keys=0',
+		url: 'https://kuula.co/share/collection/7J2HW?logo=-1&info=0&fs=0&vr=0&zoom=1&gyro=0&initload=0&thumbs=1&keys=0',
 	},
 	{
 		id: '3',
 		name: 'Hall Cottage',
-		url: 'https://kuula.co/share/collection/7JBXf?logo=-1&info=0&fs=0&vr=0&gyro=0&initload=0&thumbs=1&keys=0',
+		url: 'https://kuula.co/share/collection/7JBXf?logo=-1&info=0&fs=0&vr=0&zoom=1&gyro=0&initload=0&thumbs=1&keys=0',
 	},
 	{
 		id: '4',
 		name: 'Dale Farm',
-		url: 'https://kuula.co/share/collection/7JrN5?logo=-1&info=0&fs=1&vr=0&zoom=1&gyro=0&initload=0&thumbs=1',
+		url: 'https://kuula.co/share/collection/7JrN5?logo=-1&info=0&fs=0&vr=0&zoom=1&gyro=0&initload=0&thumbs=1&keys=0',
 	},
 ];
