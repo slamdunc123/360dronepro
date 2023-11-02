fe751db74a1fa10edb4345fc390169ebc822eff5
duncanlaidlaw <slamdunc123@gmail.com>
2023-11-02 15:55:22 +0000

    feature - created initial footer component

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className='bg-sky-700 mt-auto'>
+			<div className='p-4 text-center text-white '>
+				© 2023 Copyright: 360dronepro
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
