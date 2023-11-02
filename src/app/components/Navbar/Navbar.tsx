4bc6d21f6023d4ef099e7bcdbe4cb059c1060eb7
duncanlaidlaw <slamdunc123@gmail.com>
2023-11-02 12:07:19 +0000

    fix - close mobile menu when resizing to a mobile screen,

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,17 +1,35 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { MdOutlineMenu } from 'react-icons/md';
 import { MdOutlineClose } from 'react-icons/md';
 import Image from 'next/image';
 
 const Navbar = () => {
-	const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [logoWidth, setLogoWidth] = useState<string>('192');
 
 	const handleMenuOnClick = () => {
 		setIsOpen((prevIsOpen) => !prevIsOpen);
 	};
+
+	const handleResize = () => {
+		if (window.innerWidth >= 640) {
+			setIsOpen(false);
+			setLogoWidth('256');
+		} else {
+			setLogoWidth('192');
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
 	return (
 		<header className='sticky top-0 left-0 w-full'>
 			<nav className='flex items-center justify-between flex-wrap bg-white shadow-md px-4 py-6'>
@@ -19,7 +37,7 @@ const Navbar = () => {
 					<Image
 						src='/360dronepro-logo.svg'
 						alt=''
-						width='256'
+						width={Number(logoWidth)}
 						height='0'
 					/>
 				</div>
