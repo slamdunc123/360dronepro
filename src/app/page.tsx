381f09e8853c30a451b9e3f2e9598eb0246071cb
duncanlaidlaw <slamdunc123@gmail.com>
2023-11-07 14:08:45 +0000

    update - add styling and links to app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Page() {
 	return (
@@ -17,32 +18,41 @@ export default function Page() {
 				<h2 className='text-2xl font-bold text-sky-700 my-4'>
 					Services
 				</h2>
-				<div className='flex flex-col sm:flex-row justify-between w-full '>
-					<div className='flex-1 flex flex-col items-center'>
-						<h3 className='mb-4 text-lg'>Virtual Tours</h3>
-						<Image
-							src='/360.svg'
-							alt=''
-							width='0'
-							height='0'
-							className='w-32'
-						/>
-						<p>
-							Enhance the viewer&apos;s experience with a virtual tour
-						</p>
+				<div className='flex flex-col sm:flex-row justify-between w-full space-y-8 sm:space-y-0'>
+					<div className='flex-1 flex justify-center'>
+						<Link
+							href='/virtual-tours'
+							className='flex flex-col items-center border-1 w-11/12 py-4 px-2 shadow-md hover:bg-slate-300 cursor-pointer'
+						>
+							<h3 className='mb-4 text-lg'>Virtual Tours</h3>
+							<Image
+								src='/360.svg'
+								alt=''
+								width='0'
+								height='0'
+								className='w-32'
+							/>
+							<p>
+								Enhance the viewer&apos;s experience with a
+								virtual tour
+							</p>
+						</Link>
 					</div>
-					<div className='flex-1 flex flex-col items-center border-2'>
-						<h3 className='mb-4 text-lg'>Aerial Images</h3>
-						<Image
-							src='/drone.svg'
-							alt=''
-							width='0'
-							height='0'
-							className='w-32'
-						/>
-						<p>
-							Get a bird&apos;s eye view with aerial images
-						</p>
+					<div className='flex-1 flex justify-center'>
+						<Link
+							href='/aerial-images'
+							className='flex flex-col items-center border-1 w-11/12 py-4 px-2 shadow-md hover:bg-slate-300 cursor-pointer'
+						>
+							<h3 className='mb-4 text-lg'>Aerial Images</h3>
+							<Image
+								src='/drone.svg'
+								alt=''
+								width='0'
+								height='0'
+								className='w-32'
+							/>
+							<p>Get a bird&apos;s eye view with aerial images</p>
+						</Link>
 					</div>
 				</div>
 			</section>
