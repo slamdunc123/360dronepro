e34fa02b62240ca2852eeec77ac73c3ea2874644
duncanlaidlaw <slamdunc123@gmail.com>
2023-11-07 09:38:09 +0000

    update - change articles to sections, add initial content to app page (Home)

--- src/app/components/Tours/Tours.tsx
+++ src/app/components/Tours/Tours.tsx
@@ -34,7 +34,7 @@ const toursInfo = [
 const Tours = ({ tours }) => {
 	return (
 		<main className='flex flex-col w-full'>
-			<article className='p-4'>
+			<section className='p-4 shadow-sm'>
 				<h2 className='text-4xl font-bold text-sky-700 my-4'>
 					Virtual Tours
 				</h2>
@@ -47,10 +47,11 @@ const Tours = ({ tours }) => {
 					degrees perspective.
 				</p>
 				<p className='mb-2'>
-					Check out some of our client virtual tours below and see what they have to say.
+					Check out some of our virtual tours below and see what our
+					clients have to say.
 				</p>
-			</article>
-			<article className='p-4'>
+			</section>
+			<section className='p-4 shadow-sm'>
 				<h2 className='text-2xl font-bold text-sky-700 my-4'>
 					Contact Us
 				</h2>
@@ -61,61 +62,61 @@ const Tours = ({ tours }) => {
 				</p>
 				<p className='mb-2'>
 					To discuss your requirements please contact us on -{' '}
-					<span className='text-lg text-sky-700'>
-						07968903859
-					</span>.
+					<span className='text-lg text-sky-700'>07968903859</span>.
 				</p>
-			</article>
-			{tours &&
-				tours.map((item, index) => {
-					return (
-						<div
-							className={`p-2 md:flex shadow-md border-slate-200 border m-2 hover:bg-slate-300 hover:text-black ${
-								index % 2
-									? 'flex-row-reverse bg-slate-100 hover:bg-sky-100'
-									: 'bg-slate-200 '
-							}`}
-							key={item.id}
-						>
+			</section>
+			<section className='p-4'>
+				{tours &&
+					tours.map((item, index) => {
+						return (
 							<div
-								className={`flex flex-col w-full md:w-1/2 ${
+								className={`md:flex shadow-md border-slate-200 border m-2 hover:bg-slate-300 hover:text-black ${
 									index % 2
-										? 'justify-self-end'
-										: 'justify-self-start'
+										? 'flex-row-reverse bg-slate-100 hover:bg-sky-100'
+										: 'bg-slate-200 '
 								}`}
+								key={item.id}
 							>
-								<Link href={`/virtual-tours/${item.id}`}>
-									<Image
-										src={toursInfo[index].img}
-										width='0'
-										height='0'
-										className='w-full'
-										unoptimized
-										alt={toursInfo[index].name}
-									/>
-								</Link>
-							</div>
-							<div
-								className={`flex flex-col w-full md:w-1/2 lg:mt-6 xl:mt-12 ${
-									index % 2
-										? 'justify-self-start'
-										: 'justify-self-end'
-								}`}
-							>
-								<Link href={`/virtual-tours/${item.id}`}>
-									<div className='flex flex-col items-center justify-center p-4'>
-										<div className='self-start mb-2'>
-											{item.name}
+								<div
+									className={`flex flex-col w-full md:w-1/2 ${
+										index % 2
+											? 'justify-self-end'
+											: 'justify-self-start'
+									}`}
+								>
+									<Link href={`/virtual-tours/${item.id}`}>
+										<Image
+											src={toursInfo[index].img}
+											width='0'
+											height='0'
+											className='w-full'
+											unoptimized
+											alt={toursInfo[index].name}
+										/>
+									</Link>
+								</div>
+								<div
+									className={`flex flex-col w-full md:w-1/2 lg:mt-6 xl:mt-12 ${
+										index % 2
+											? 'justify-self-start'
+											: 'justify-self-end'
+									}`}
+								>
+									<Link href={`/virtual-tours/${item.id}`}>
+										<div className='flex flex-col items-center justify-center p-4'>
+											<div className='self-start mb-2'>
+												{item.name}
+											</div>
+											<p className='italic'>
+												{`${toursInfo[index].testimonial} - ${toursInfo[index].owner}`}
+											</p>
 										</div>
-										<p className='italic'>
-											{`${toursInfo[index].testimonial} - ${toursInfo[index].owner}`}
-										</p>
-									</div>
-								</Link>
+									</Link>
+								</div>
 							</div>
-						</div>
-					);
-				})}
+						);
+					})}
+			</section>
 		</main>
 	);
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 export default function Page() {
 	return (
 		<main className='flex flex-col w-full'>
-			<article className='p-4'>
-				<p>Home page content...</p>
-			</article>
+			<section className='p-4 shadow-sm'>
+				<h2 className='text-4xl font-bold text-sky-700 my-4'>
+					360dronepro
+				</h2>
+				<p className='mb-2'>
+					At 360dronepro we use 360 camera and drone technology to
+					provide virtual tour and aerial image services for our
+					clients.
+				</p>
+			</section>
+			<section className='p-4 shadow-sm'>
+				<h2 className='text-2xl font-bold text-sky-700 my-4'>
+					Services
+				</h2>
+				<div className='flex flex-col sm:flex-row justify-between w-full '>
+					<div className='flex-1 flex sm:justify-center'>
+						<h3>Virtual Tours</h3>
+					</div>
+					<div className='flex-1 flex sm:justify-center'>
+						Aerial Images
+					</div>
+				</div>
+			</section>
 		</main>
 	);
 }
