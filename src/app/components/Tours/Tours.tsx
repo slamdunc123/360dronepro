77f553de5d879a2d55912e15e9ded6ca1f67d151
duncanlaidlaw <slamdunc123@gmail.com>
2023-11-07 17:01:32 +0000

    temp - comment out testimonials and owner until have real data

--- src/app/components/Tours/Tours.tsx
+++ src/app/components/Tours/Tours.tsx
@@ -13,21 +13,21 @@ const toursInfo = [
 	},
 	{
 		img: 'hall-cottage.png',
-		testimonial:
-			'...lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum voluptate porro quam quasi voluptas nostrum optio accusamus in eius totam tempora illo itaque, maxime quae, a animi nihil, enim impedit.',
-		owner: 'Susannah',
+		// testimonial:
+		// 	'...lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum voluptate porro quam quasi voluptas nostrum optio accusamus in eius totam tempora illo itaque, maxime quae, a animi nihil, enim impedit.',
+		// owner: 'Susannah',
 	},
 	{
 		img: 'dale-farm.png',
-		testimonial:
-			'...lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum voluptate porro quam quasi voluptas nostrum optio accusamus in eius totam tempora illo itaque, maxime quae, a animi nihil, enim impedit.',
-		owner: 'Duncan',
+		// testimonial:
+		// 	'...lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum voluptate porro quam quasi voluptas nostrum optio accusamus in eius totam tempora illo itaque, maxime quae, a animi nihil, enim impedit.',
+		// owner: 'Duncan',
 	},
 	{
 		img: 'dale-view.png',
-		testimonial:
-			'...lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum voluptate porro quam quasi voluptas nostrum optio accusamus in eius totam tempora illo itaque, maxime quae, a animi nihil, enim impedit.',
-		owner: 'Christine',
+		// testimonial:
+		// 	'...lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum voluptate porro quam quasi voluptas nostrum optio accusamus in eius totam tempora illo itaque, maxime quae, a animi nihil, enim impedit.',
+		// owner: 'Christine',
 	},
 ];
 
@@ -108,7 +108,7 @@ const Tours = ({ tours }) => {
 												{item.name}
 											</div>
 											<p className='italic'>
-												{`${toursInfo[index].testimonial} - ${toursInfo[index].owner}`}
+												{toursInfo[index].testimonial && toursInfo[index].owner ? `${toursInfo[index].testimonial} - ${toursInfo[index].owner}` : null}
 											</p>
 										</div>
 									</Link>
