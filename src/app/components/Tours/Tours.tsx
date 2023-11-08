2187608df7ce65fa31ab0545d2fbba8bba3d7fec
duncanlaidlaw <slamdunc123@gmail.com>
2023-11-08 08:54:52 +0000

    refactor - move text sections from Tours component to virtual-tours page

--- src/app/components/Tours/Tours.tsx
+++ src/app/components/Tours/Tours.tsx
@@ -7,94 +7,51 @@ import { toursInfo } from './toursInfo';
 
 const Tours = ({ tours }: ToursPropsType) => {
 	return (
-		<main className='flex flex-col w-full'>
-			<section className='p-4 shadow-sm'>
-				<h2 className='text-4xl font-bold text-sky-700 my-4'>
-					Virtual Tours
-				</h2>
-				<p className='mb-2'>
-					Showcase your property or business premises online using the
-					latest virtual tour software.
-				</p>
-				<p className='mb-2'>
-					Enable viewers to navigate and view each location in a 360
-					degrees perspective.
-				</p>
-				<p className='mb-2'>
-					Check out some of our virtual tours below and see what our
-					clients have to say.
-				</p>
-			</section>
-			<section className='p-4 shadow-sm'>
-				<h2 className='text-2xl font-bold text-sky-700 my-4'>
-					Contact Us
-				</h2>
-				<p className='mb-2'>
-					360dronepro provide a virtual tour service, where we work
-					closely with our clients to meet their individual, virtual
-					tour needs.
-				</p>
-				<p className='mb-2'>
-					To discuss your requirements please contact us on -{' '}
-					<span className='text-lg text-sky-700'>07968903859</span>.
-				</p>
-			</section>
-			<section className='p-4 space-y-4'>
-				{tours &&
-					tours.map((item, index) => {
-						return (
-							<div
-								className={`lg:flex shadow-md border-slate-200 border hover:bg-slate-300 hover:text-black ${
-									index % 2
-										? 'flex-row-reverse bg-slate-100 hover:bg-sky-100'
-										: 'bg-slate-200 '
-								}`}
-								key={item?.id}
-							>
-								<div
-									className={`flex flex-col w-full lg:w-1/2 ${
-										index % 2
-											? 'justify-self-end'
-											: 'justify-self-start'
-									}`}
-								>
-									<Link href={`/virtual-tours/${item?.id}`}>
-										<Image
-											src={`/${toursInfo[index].img}`}
-											width='0'
-											height='0'
-											className='w-full'
-											unoptimized
-											alt={toursInfo[index].img}
-										/>
-									</Link>
-								</div>
-								<div
-									className={`flex flex-col w-full md:w-1/2 lg:mt-6 xl:mt-12 ${
-										index % 2
-											? 'justify-self-start'
-											: 'justify-self-end'
-									}`}
-								>
-									<Link href={`/virtual-tours/${item?.id}`}>
-										<div className='flex flex-col items-center justify-center p-4'>
-											<div className='self-start mb-2'>
-												{item?.name}
-											</div>
-											<p className='italic'>
-												{toursInfo[index].testimonial &&
-												toursInfo[index].owner
-													? `${toursInfo[index].testimonial} - ${toursInfo[index].owner}`
-													: null}
-											</p>
-										</div>
-									</Link>
-								</div>
-							</div>
-						);
-					})}
-			</section>
-		</main>
+		tours &&
+		tours.map((item, index) => (
+			<div
+				className={`lg:flex shadow-md border-slate-200 border hover:bg-slate-300 hover:text-black ${
+					index % 2
+						? 'flex-row-reverse bg-slate-100 hover:bg-sky-100'
+						: 'bg-slate-200 '
+				}`}
+				key={item?.id}
+			>
+				<div
+					className={`flex flex-col w-full lg:w-1/2 ${
+						index % 2 ? 'justify-self-end' : 'justify-self-start'
+					}`}
+				>
+					<Link href={`/virtual-tours/${item?.id}`}>
+						<Image
+							src={`/${toursInfo[index].img}`}
+							width='0'
+							height='0'
+							className='w-full'
+							unoptimized
+							alt={toursInfo[index].img}
+						/>
+					</Link>
+				</div>
+				<div
+					className={`flex flex-col w-full md:w-1/2 lg:mt-6 xl:mt-12 ${
+						index % 2 ? 'justify-self-start' : 'justify-self-end'
+					}`}
+				>
+					<Link href={`/virtual-tours/${item?.id}`}>
+						<div className='flex flex-col items-center justify-center p-4'>
+							<div className='self-start mb-2'>{item?.name}</div>
+							<p className='italic'>
+								{toursInfo[index].testimonial &&
+								toursInfo[index].owner
+									? `${toursInfo[index].testimonial} - ${toursInfo[index].owner}`
+									: null}
+							</p>
+						</div>
+					</Link>
+				</div>
+			</div>
+		))
 	);
 };
 
--- src/app/virtual-tours/page.tsx
+++ src/app/virtual-tours/page.tsx
@@ -9,7 +9,6 @@ import Tours from '../components/Tours/Tours';
 import awsExports from '@/aws-exports';
 Amplify.configure({ ...awsExports, ssr: true });
 
-
 const fetchTours = async () => {
 	const tours = await API.graphql<GraphQLQuery<ListToursQuery>>({
 		query: listTours,
@@ -20,7 +19,44 @@ const fetchTours = async () => {
 const Page = async () => {
 	const fetchedTours = await fetchTours();
 	const tours = fetchedTours.data?.listTours?.items;
-	return <Tours tours={tours} />;
+	return (
+		<main className='flex flex-col w-full'>
+			<section className='p-4 shadow-sm'>
+				<h2 className='text-4xl font-bold text-sky-700 my-4'>
+					Virtual Tours
+				</h2>
+				<p className='mb-2'>
+					Showcase your property or business premises online using the
+					latest virtual tour software.
+				</p>
+				<p className='mb-2'>
+					Enable viewers to navigate and view each location in a 360
+					degrees perspective.
+				</p>
+				<p className='mb-2'>
+					Check out some of our virtual tours below and see what our
+					clients have to say.
+				</p>
+			</section>
+			<section className='p-4 shadow-sm'>
+				<h2 className='text-2xl font-bold text-sky-700 my-4'>
+					Contact Us
+				</h2>
+				<p className='mb-2'>
+					360dronepro provide a virtual tour service, where we work
+					closely with our clients to meet their individual, virtual
+					tour needs.
+				</p>
+				<p className='mb-2'>
+					To discuss your requirements please contact us on -{' '}
+					<span className='text-lg text-sky-700'>07968903859</span>.
+				</p>
+			</section>
+			<section className='p-4 space-y-4'>
+				<Tours tours={tours} />;
+			</section>
+		</main>
+	);
 };
 
 export default Page;
