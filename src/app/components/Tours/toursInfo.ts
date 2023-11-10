e94b419bb5f64248f82711434b4af964974c9b8f
duncanlaidlaw <slamdunc123@gmail.com>
2023-11-10 16:56:54 +0000

    update - add further testimonials

--- src/app/components/Tours/toursInfo.ts
+++ src/app/components/Tours/toursInfo.ts
@@ -2,25 +2,25 @@ export const toursInfo = [
 	{
 		img: 'rock-cottage.png',
 		testimonial:
-			'"Absolutely thrilled with the virtual tour of our holiday cottage. It gives a lovely feel of the place. The service was most professional and creative."',
+			'"Absolutely thrilled with the virtual tour of our holiday cottage. It gives a lovely feel of the place. 360dronepro were most professional and creative."',
 		owner: 'Denise',
 	},
 	{
 		img: 'hall-cottage.png',
-		// testimonial:
-		// 	'...lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum voluptate porro quam quasi voluptas nostrum optio accusamus in eius totam tempora illo itaque, maxime quae, a animi nihil, enim impedit.',
-		// owner: 'Susannah',
+		testimonial:
+			'"Really please with the service we received and I couldn\'t be happier with the virtual tour. It feels like you\'re actually at the cottage."',
+		owner: 'Susannah',
 	},
 	{
 		img: 'dale-farm.png',
-		// testimonial:
-		// 	'...lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum voluptate porro quam quasi voluptas nostrum optio accusamus in eius totam tempora illo itaque, maxime quae, a animi nihil, enim impedit.',
-		// owner: 'Duncan',
+		testimonial:
+			'"Wow! The virtual tour 360dronepro created is excellent and has easily exceeded my expectations. They are professional, reliable, and the end result is great."',
+		owner: 'Duncan',
 	},
 	{
 		img: 'dale-view.png',
-		// testimonial:
-		// 	'...lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum voluptate porro quam quasi voluptas nostrum optio accusamus in eius totam tempora illo itaque, maxime quae, a animi nihil, enim impedit.',
-		// owner: 'Christine',
+		testimonial:
+			'"Many thanks for taking the fantastic aerial shots and for creating the virtual tour. It will provide future guests with an excellent all round view of the cottage and its surroundings."',
+		owner: 'Christine',
 	},
 ];
