e759952cea4487d325b95826e25960b2ee43e2cc
duncanlaidlaw <slamdunc123@gmail.com>
2023-12-05 15:39:12 +0000

    update - toursInfo file

--- src/app/components/Tours/toursInfo.ts
+++ src/app/components/Tours/toursInfo.ts
@@ -1,23 +1,31 @@
 export const toursInfo = [
 	{
+    id: '95c3d7fd-dc2b-4038-b4df-d0d38c4c00af',
+    name: 'Rock Cottage',
 		img: 'rock-cottage.png',
 		testimonial:
 			'"Absolutely thrilled with the virtual tour of our holiday cottage. It gives a lovely feel of the place. 360dronepro were most professional and creative."',
 		owner: 'Denise',
 	},
 	{
+    id: '4353dd2f-1ec0-438a-847d-e022b4f7eaf0',
+    name: 'Hall Cottage',
 		img: 'hall-cottage.png',
 		testimonial:
 			'"Really pleased with the service we received and I couldn\'t be happier with the virtual tour. It feels like you\'re actually at the cottage."',
 		owner: 'Susannah',
 	},
 	{
+    id: '8ff41d2e-e0a8-4e56-935f-fb5219652498',
+    name: 'Dale Farm',
 		img: 'dale-farm.png',
 		testimonial:
 			'"Wow! The virtual tour 360dronepro created is excellent and has easily exceeded my expectations. They are professional, reliable, and the end result is great."',
 		owner: 'Duncan',
 	},
 	{
+    id: 'df7afc36-eb37-41ef-8bb4-fcf168f5389b',
+    name: 'Dale View',
 		img: 'dale-view.png',
 		testimonial:
 			'"Many thanks for taking the fantastic aerial shots and for creating the virtual tour. It will provide future guests with an excellent all round view of the cottage and its surroundings."',
