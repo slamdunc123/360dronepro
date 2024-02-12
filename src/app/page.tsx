7bfee89e6b1cec8cfede90d1f260840a67d0f4e7
duncanlaidlaw <slamdunc123@gmail.com>
2024-02-12 15:55:54 +0000

    add drone inspections icon, activate links to drone inspections page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,60 +12,70 @@ const Home = () => {
 					clients.
 				</p>
 			</section>
-			<h2 className='text-2xl font-bold my-4 text-sky-700'>Services</h2>
+			<section className='p-4 shadow-sm text-sky-700'>
+				<h2 className='text-2xl font-bold my-4 text-sky-700'>
+					Services
+				</h2>
+			</section>
 			<section className='p-4 shadow-sm text-sky-700 flex justify-center'>
-				<div className='flex flex-col w-full items-center gap-y-4 sm:gap-x-2'>
-					<div className='flex-1 flex justify-center w-full'>
+				<div className='flex flex-col md:flex-row w-full items-center gap-y-4 sm:gap-x-2 '>
+					<div className='flex-1 flex justify-center w-full h-64'>
 						<Link
 							href='/virtual-tours'
 							className='flex flex-col items-center border-1 w-full py-4 px-2 shadow-md hover:bg-slate-300 cursor-pointer'
 						>
 							<h3 className='mb-4 text-lg'>Virtual Tours</h3>
-							<Image
-								src='/360.svg'
-								alt='360.svg'
-								width='0'
-								height='0'
-								className='w-32'
-							/>
+							<div className='w-32 h-32 flex flex-col items-center justify-center'>
+								<Image
+									src='/360.svg'
+									alt='360.svg'
+									width='128'
+									height='128'
+								/>
+							</div>
 							<p>
 								Enhance the viewer&apos;s experience with a
 								virtual tour
 							</p>
 						</Link>
 					</div>
-					<div className='flex-1 flex justify-center w-full'>
+					<div className='flex-1 flex justify-center w-full h-64'>
 						<Link
 							href='/aerial-images'
 							className='flex flex-col items-center border-1 w-full py-4 px-2 shadow-md hover:bg-slate-300 cursor-pointer'
 						>
 							<h3 className='mb-4 text-lg'>Aerial Images</h3>
-							<Image
-								src='/drone.svg'
-								alt='drone.svg'
-								width='0'
-								height='0'
-								className='w-32'
-							/>
+							<div className='w-32 h-32 flex flex-col items-center justify-center'>
+								<Image
+									src='/drone.svg'
+									alt='drone.svg'
+									width='128'
+									height='128'
+								/>
+							</div>
 							<p>Get a bird&apos;s eye view with aerial images</p>
 						</Link>
 					</div>
-					{/* <div className='flex-1 flex justify-center w-full'>
+					<div className='flex-1 flex justify-center w-full h-64'>
 						<Link
 							href='/drone-inspections'
 							className='flex flex-col items-center border-1 w-full py-4 px-2 shadow-md hover:bg-slate-300 cursor-pointer'
 						>
 							<h3 className='mb-4 text-lg'>Drone Inspections</h3>
-							<Image
-								src='/drone.svg'
-								alt='drone.svg'
-								width='0'
-								height='0'
-								className='w-32'
-							/>
-							<p>Check for any roofing issues with a drone inspection</p>
+							<div className='w-32 h-32 flex flex-col items-center justify-center'>
+								<Image
+									src='/magnifier.svg'
+									alt='magnifier.svg'
+									width='96'
+									height='96'
+								/>
+							</div>
+							<p>
+								Check for any roofing issues with a drone
+								inspection
+							</p>
 						</Link>
-					</div> */}
+					</div>
 				</div>
 			</section>
 		</main>
