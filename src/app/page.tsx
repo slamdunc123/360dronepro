3c8c68f560af8430dee7d13f09a746c8b9256bc5
duncanlaidlaw <slamdunc123@gmail.com>
2024-03-22 04:22:36 +0000

    update wording on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ const Home = () => {
 				<h1 className='text-4xl font-bold my-4'>360dronepro</h1>
 				<p className='mb-2'>
 					At 360dronepro we use 360 camera and drone technology to
-					provide virtual tours, aerial images, and drone inspection services for our
-					clients.
+					provide virtual tours, aerial images, and drone inspection
+					services for our clients.
 				</p>
 			</section>
 			<section className='p-4 shadow-sm text-sky-700'>
@@ -19,7 +19,7 @@ const Home = () => {
 			</section>
 			<section className='p-4 shadow-sm text-sky-700 flex justify-center'>
 				<div className='flex flex-col md:flex-row w-full items-center gap-y-4 sm:gap-x-2 '>
-					<div className='flex-1 flex justify-center w-full h-64'>
+					<div className='flex-1 flex justify-center w-full h-96'>
 						<Link
 							href='/virtual-tours'
 							className='flex flex-col items-center border-1 w-full py-4 px-2 shadow-md hover:bg-slate-300 cursor-pointer'
@@ -33,13 +33,20 @@ const Home = () => {
 									height='128'
 								/>
 							</div>
-							<p>
-								Enhance the viewer&apos;s experience with a
-								virtual tour
-							</p>
+							<div className='justify-start w-11/12'>
+								<p>
+									Enhance the viewer&apos;s experience with a
+									virtual tour
+								</p>
+								<p>. Virtual walk through</p>
+								<p>. User interaction</p>
+								<p>. Increase engagement</p>
+								<p>. Stand out from the crowd</p>
+								<p>. Reach a wider audience</p>
+							</div>
 						</Link>
 					</div>
-					<div className='flex-1 flex justify-center w-full h-64'>
+					<div className='flex-1 flex justify-center w-full h-96'>
 						<Link
 							href='/aerial-images'
 							className='flex flex-col items-center border-1 w-full py-4 px-2 shadow-md hover:bg-slate-300 cursor-pointer'
@@ -53,10 +60,20 @@ const Home = () => {
 									height='128'
 								/>
 							</div>
-							<p>Get a bird&apos;s eye view with aerial images</p>
+							<div className='justify-start w-11/12'>
+								<p>
+									Get a bird&apos;s eye view with aerial
+									images
+								</p>
+								<p>. Bird’s eye views</p>
+								<p>. Overhead perspective</p>
+								<p>. Capture scale</p>
+								<p>. Showcase full exterior</p>
+								<p>. Boost portfolio</p>
+							</div>
 						</Link>
 					</div>
-					<div className='flex-1 flex justify-center w-full h-64'>
+					<div className='flex-1 flex justify-center w-full h-96'>
 						<Link
 							href='/drone-inspections'
 							className='flex flex-col items-center border-1 w-full py-4 px-2 shadow-md hover:bg-slate-300 cursor-pointer'
@@ -70,10 +87,17 @@ const Home = () => {
 									height='96'
 								/>
 							</div>
-							<p>
-								Check for any roofing issues with a drone
-								inspection
-							</p>
+							<div className='justify-start w-11/12'>
+								<p>
+									Check for any roofing issues with a drone
+									inspection
+								</p>
+								<p>. Blocked gutters</p>
+								<p>. Mortar decay</p>
+								<p>. Cracked tiles</p>
+								<p>. Loose flashing</p>
+								<p>. Water ingress</p>
+							</div>
 						</Link>
 					</div>
 				</div>
