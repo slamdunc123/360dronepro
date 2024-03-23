86b9813953a14d0cbaef13ba51ecde2831f08a37
duncanlaidlaw <slamdunc123@gmail.com>
2024-03-23 12:29:59 +0000

    reverse colours for page headings

--- src/app/aerial-images/page.tsx
+++ src/app/aerial-images/page.tsx
@@ -1,42 +1,33 @@
-import Aerials from '../components/Aerials/Aerials';
+import Aerials from "../components/Aerials/Aerials";
 
 const AerialImages = () => {
-	return (
-		<main className='flex flex-col w-full'>
-			<section className='p-4 shadow-sm text-sky-700'>
-				<h1 className='text-4xl font-bold my-4'>
-					Aerial Images
-				</h1>
-				<p className='mb-2'>
-					Capture your property or business premises from a unique
-					aerial perspective using drone photography.
-				</p>
-				<p className='mb-2'>
-					Add aerial images to your property portfolio.
-				</p>
-				<p className='mb-2'>
-					Check out some of our aerial images below.
-				</p>
-			</section>
-			<section className='p-4 shadow-sm text-sky-700'>
-				<h2 className='text-2xl font-bold my-4'>
-					Contact Us
-				</h2>
-				<p className='mb-2'>
-					360dronepro provide an aerial image service, where we work
-					closely with our clients to meet their individual, aerial
-					image needs.
-				</p>
-				<p className='mb-2'>
-					To discuss your requirements please contact us on -{' '}
-					<span className='text-lg text-sky-700'>07968903859</span>.
-				</p>
-			</section>
-			<section className='p-4 shadow-sm'>
-				<Aerials />
-			</section>
-		</main>
-	);
+  return (
+    <main className="flex w-full flex-col">
+      <section className="bg-sky-700 p-4 text-white shadow-sm">
+        <h1 className="my-4 text-4xl font-bold">Aerial Images</h1>
+        <p className="mb-2">
+          Capture your property or business premises from a unique aerial
+          perspective using drone photography.
+        </p>
+        <p className="mb-2">Add aerial images to your property portfolio.</p>
+        <p className="mb-2">Check out some of our aerial images below.</p>
+      </section>
+      <section className="p-4 text-sky-700 shadow-sm">
+        <h2 className="my-4 text-2xl font-bold">Contact Us</h2>
+        <p className="mb-2">
+          360dronepro provide an aerial image service, where we work closely
+          with our clients to meet their individual, aerial image needs.
+        </p>
+        <p className="mb-2">
+          To discuss your requirements please contact us on -{" "}
+          <span className="text-lg text-sky-700">07968903859</span>.
+        </p>
+      </section>
+      <section className="p-4 shadow-sm">
+        <Aerials />
+      </section>
+    </main>
+  );
 };
 
 export default AerialImages;
--- src/app/virtual-tours/page.tsx
+++ src/app/virtual-tours/page.tsx
@@ -1,58 +1,57 @@
-import React from 'react';
-import { Amplify, API } from 'aws-amplify';
-import { GraphQLQuery } from '@aws-amplify/api';
-import { ListToursQuery } from '../../API';
-import { listTours } from '../../graphql/queries';
+import React from "react";
+import { Amplify, API } from "aws-amplify";
+import { GraphQLQuery } from "@aws-amplify/api";
+import { ListToursQuery } from "../../API";
+import { listTours } from "../../graphql/queries";
 
-import Tours from '../components/Tours/Tours';
+import Tours from "../components/Tours/Tours";
 
-import awsExports from '@/aws-exports';
+import awsExports from "@/aws-exports";
 Amplify.configure({ ...awsExports, ssr: true });
 
 const fetchTours = async () => {
-	const tours = await API.graphql<GraphQLQuery<ListToursQuery>>({
-		query: listTours,
-	});
-	return tours;
+  const tours = await API.graphql<GraphQLQuery<ListToursQuery>>({
+    query: listTours,
+  });
+  return tours;
 };
 
 const VirtualTours = async () => {
-	const fetchedTours = await fetchTours();
-	const tours = fetchedTours.data?.listTours?.items;
-	return (
-		<main className='flex flex-col w-full'>
-			<section className='p-4 shadow-sm text-sky-700'>
-				<h1 className='text-4xl font-bold  my-4'>Virtual Tours</h1>
-				<p className='mb-2'>
-					Showcase your property or business premises online using the
-					latest virtual tour software.
-				</p>
-				<p className='mb-2'>
-					Enable viewers to navigate and view each location from a 360
-					degrees perspective.
-				</p>
-				<p className='mb-2'>
-					Check out some of our virtual tours below and see what our
-					clients have to say.
-				</p>
-			</section>
-			<section className='p-4 shadow-sm text-sky-700'>
-				<h2 className='text-2xl font-bold my-4'>Contact Us</h2>
-				<p className='mb-2'>
-					360dronepro provide a virtual tour service, where we work
-					closely with our clients to meet their individual, virtual
-					tour needs.
-				</p>
-				<p className='mb-2'>
-					To discuss your requirements please contact us on -{' '}
-					<span className='text-lg text-sky-700'>07968903859</span>.
-				</p>
-			</section>
-			<section className='p-4 space-y-4'>
-				<Tours tours={tours} />
-			</section>
-		</main>
-	);
+  const fetchedTours = await fetchTours();
+  const tours = fetchedTours.data?.listTours?.items;
+  return (
+    <main className="flex w-full flex-col">
+      <section className="bg-sky-700 p-4 text-white shadow-sm">
+        <h1 className="my-4 text-4xl  font-bold">Virtual Tours</h1>
+        <p className="mb-2">
+          Showcase your property or business premises online using the latest
+          virtual tour software.
+        </p>
+        <p className="mb-2">
+          Enable viewers to navigate and view each location from a 360 degrees
+          perspective.
+        </p>
+        <p className="mb-2">
+          Check out some of our virtual tours below and see what our clients
+          have to say.
+        </p>
+      </section>
+      <section className="p-4 text-sky-700 shadow-sm">
+        <h2 className="my-4 text-2xl font-bold">Contact Us</h2>
+        <p className="mb-2">
+          360dronepro provide a virtual tour service, where we work closely with
+          our clients to meet their individual, virtual tour needs.
+        </p>
+        <p className="mb-2">
+          To discuss your requirements please contact us on -{" "}
+          <span className="text-lg text-sky-700">07968903859</span>.
+        </p>
+      </section>
+      <section className="space-y-4 p-4">
+        <Tours tours={tours} />
+      </section>
+    </main>
+  );
 };
 
 export default VirtualTours;
