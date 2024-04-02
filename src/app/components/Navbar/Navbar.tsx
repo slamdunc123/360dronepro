c535a47cdffd868f5760c32153abd7776080ac16
duncanlaidlaw <slamdunc123@gmail.com>
2024-04-02 13:41:15 +0100

    fix virtual tour images not showing in Safari

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-import { MdOutlineMenu } from "react-icons/md";
-import { MdOutlineClose } from "react-icons/md";
+import { MdOutlineMenu } from 'react-icons/md';
+import { MdOutlineClose } from 'react-icons/md';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -15,62 +15,62 @@ const Navbar = () => {
   };
 
   return (
-    <header className="sticky left-0 top-0 z-10 w-full">
+    <header className='sticky left-0 top-0 z-10 w-full'>
       <nav
-        className="flex flex-wrap items-center justify-between bg-white p-4 shadow-md"
-        role="navigation"
-        aria-label="main navigation menu"
+        className='flex flex-wrap items-center justify-between bg-white p-4 shadow-md '
+        role='navigation'
+        aria-label='main navigation menu'
       >
-        <div className="flex flex-shrink-0 items-center text-sky-700">
+        <div className='flex flex-shrink-0 items-center text-sky-700'>
           <Image
-            src="/360dronepro-logo.svg"
-            alt="360 drone pro company logo"
-            width="0"
-            height="0"
-            className="w-64"
+            src='/360dronepro-logo.svg'
+            alt='360 drone pro company logo'
+            width='0'
+            height='0'
+            className='w-64'
           />
         </div>
-        <div className="block lg:hidden">
+        <div className='block lg:hidden'>
           <button
-            className="text-2xl text-sky-700 lg:hidden"
+            className='text-2xl text-sky-700 lg:hidden'
             onClick={handleMenuOnClick}
-            type="button"
+            type='button'
             aria-expanded={isOpen}
-            aria-label="main navigation menu button"
+            aria-label='main navigation menu button'
           >
             {!isOpen ? <MdOutlineMenu /> : <MdOutlineClose />}
           </button>
         </div>
         <div
           className={`${
-            isOpen ? "block" : "hidden"
+            isOpen ? 'block' : 'hidden'
           } w-full lg:flex lg:w-auto lg:items-center`}
         >
-          <div className="mt-10 flex flex-col lg:mt-0 lg:flex-grow lg:flex-row">
+          <div className='mt-10 flex flex-col lg:mt-0 lg:flex-grow lg:flex-row'>
             <Link
-              href="/"
-              className="text-sky-700 hover:text-sky-300 lg:mr-4"
+              href='/'
+              className='text-sky-700 hover:text-sky-300 lg:mr-4'
               onClick={handleMenuOnClick}
             >
               Home
             </Link>
             <Link
-              href="/virtual-tours"
-              className="text-sky-700 hover:text-sky-300 lg:mr-4"
+              href='/virtual-tours'
+              className='text-sky-700 hover:text-sky-300 lg:mr-4'
               onClick={handleMenuOnClick}
             >
               Virtual Tours
             </Link>
             <Link
-              href="/aerial-images"
-              className="text-sky-700 hover:text-sky-300 lg:mr-4"
+              href='/aerial-images'
+              className='text-sky-700 hover:text-sky-300 lg:mr-4'
               onClick={handleMenuOnClick}
             >
               Aerial Images
             </Link>
             <Link
-              href="/drone-inspections"
-              className="text-sky-700 hover:text-sky-300"
+              href='/drone-inspections'
+              className='text-sky-700 hover:text-sky-300'
               onClick={handleMenuOnClick}
             >
               Drone Inspections
--- src/app/components/Tours/Tours.tsx
+++ src/app/components/Tours/Tours.tsx
@@ -7,44 +7,44 @@ import { toursInfo } from './toursInfo';
 
 //TODO: add toursInfo data to tours database to ensure one source of data, add fields and include a featured boolean field
 const Tours = ({ tours }: ToursPropsType) => {
-	return toursInfo.map((item, index) => (
-		<Link href={`/virtual-tours/${item.id}`} key={item?.id}>
-			<div
-				className={`md:flex shadow-md border-slate-200 border bg-slate-100 hover:bg-slate-200 ${
-					index % 2 && 'flex-row-reverse'
-				}`}
-			>
-				<div
-					className={`flex flex-col w-full md:w-1/2 ${
-						index % 2 ? 'justify-self-end' : 'justify-self-start'
-					}`}
-				>
-					<Image
-						src={`/${item.img}`}
-						alt={item.alt}
-						width='0'
-						height='0'
-						className='w-full'
-						unoptimized
-					/>
-				</div>
-				<div
-					className={`flex flex-col w-full md:w-1/2 lg:mt-6 xl:mt-12 ${
-						index % 2 ? 'justify-self-start' : 'justify-self-end'
-					}`}
-				>
-					<div className='flex flex-col items-center justify-center p-4 text-sky-700'>
-						<div className='self-start mb-2'>{item.name}</div>
-						<p className='italic'>
-							{item.testimonial && item.owner
-								? `${item.testimonial} - ${item.owner}`
-								: null}
-						</p>
-					</div>
-				</div>
-			</div>
-		</Link>
-	));
+  return toursInfo.map((item, index) => (
+    <Link href={`/virtual-tours/${item.id}`} key={item?.id}>
+      <div
+        className={`border border-slate-200 bg-slate-100 shadow-md hover:bg-slate-200 md:flex ${
+          index % 2 && 'flex-row-reverse'
+        }`}
+      >
+        <div
+          className={`flex w-full flex-col md:w-1/2 ${
+            index % 2 ? 'justify-self-end' : 'justify-self-start'
+          }`}
+        >
+          <Image
+            src={`/${item.img}`}
+            alt={item.alt}
+            width={960}
+            height={480}
+            className='cursor-pointer'
+            unoptimized
+          />
+        </div>
+        <div
+          className={`flex w-full flex-col md:w-1/2 lg:mt-6 xl:mt-12 ${
+            index % 2 ? 'justify-self-start' : 'justify-self-end'
+          }`}
+        >
+          <div className='flex flex-col items-center justify-center p-4 text-sky-700'>
+            <div className='mb-2 self-start'>{item.name}</div>
+            <p className='italic'>
+              {item.testimonial && item.owner
+                ? `${item.testimonial} - ${item.owner}`
+                : null}
+            </p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  ));
 };
 
 export default Tours;
