12f09f2f7692bcbe8af004851ef007fede83e85a
duncanlaidlaw <slamdunc123@gmail.com>
2024-04-17 16:04:32 +0100

    add tests for Home page

--- src/app/Home.test.tsx
+++ src/app/Home.test.tsx
@@ -2,10 +2,21 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Home from './page';
 
-it('should render h1 element', () => {
-	render(<Home />);
+it('should render correct h1 element', () => {
+  render(<Home />);
 
-	const headingEl = screen.getByRole('heading', { name: '360dronepro' });
-	expect(headingEl).toBeInTheDocument();
+  const headingEl = screen.getByRole('heading', {
+    level: 1,
+    name: /360dronepro/i,
+  });
+  expect(headingEl).toBeInTheDocument();
 });
+it('should render correct h2 element', () => {
+  render(<Home />);
 
+  const headingEl = screen.getByRole('heading', {
+    level: 2,
+    name: /services/i,
+  });
+  expect(headingEl).toBeInTheDocument();
+});
